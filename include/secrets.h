bc0f6ea524b54a0af5a9a3c2699dc73825321068
AWhiteKnight <awhiteknight@unity-mail.de>
2021-02-20 10:34:21 +0100

    Basic setup for esp8266 and esp32

---
+++ include/secrets.h
@@ -0,0 +1,10 @@
+
+#ifndef _SECRETS_H
+#define _SECRETS_H
+  #define MESH_SSID "whateverYouLike"
+  #define MESH_PASSWORD "somethingSneaky"
+  #define MESH_PORT 5555
+  #define MESH_CHANNEL 13
+  #define WLAN_SSID "whateverItIs"
+  #define WLAN_PASSWORD "youShouldKnow"
+#endif /* _SECRETS_H */
