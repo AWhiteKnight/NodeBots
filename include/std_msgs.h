dcfc5aae5957d2bd5a71f9b2a2ed6428366d77fa
AWhiteKnight <awhiteknight@unity-mail.de>
2021-03-13 15:34:57 +0100

    remove unnecesary extern C

--- include/std_msgs.h
+++ include/std_msgs.h
@@ -1,18 +1,10 @@
 
-#ifndef _STD_MSGS_H
-#define _STD_MSGS_H
+#ifndef _STD_MSGS_H_
+#define _STD_MSGS_H_
 
 #include <stdint.h>
 #include <math.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef int8_t rc3D_t[4];
 
-#ifdef __cplusplus
-}
 #endif
-
-#endif /* _STD_MSGS_H */
